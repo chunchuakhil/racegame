dac266f3164cbeb9aab31dbb2c1822c993971997
chunchuAkhil <akhilchunchu52@gmail.com>
2024-06-07 16:28:38 +0530

    initial commit with eslint

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,14 @@
 module.exports = {
   root: true,
-  extends: '@react-native',
+  extends: [
+    '@react-native',
+    'plugin: react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['prettier', '@typescript-eslint'],
+  parser: '@typescript-eslint/parser',
+  rules: {
+    'prettier/prettier': ['error', {printWidth: 120}],
+  },
 };
---
+++ src/components/GameBoard.tsx
@@ -0,0 +1,12 @@
+import {View, Text} from 'react-native';
+import React from 'react';
+
+const GameBoard = () => {
+  return (
+    <View>
+      <Text>GameBoard</Text>
+    </View>
+  );
+};
+
+export default GameBoard;
